3ac6f0427e089e781d42f4912d7237e56083f0c2
leko <rexx0520@gmail.com>
2026-08-30 14:30:13 +0800

    Revert "Update dependencies and change Client Profile"

--- cmd/fanbox-dl/main.go
+++ cmd/fanbox-dl/main.go
@@ -189,7 +189,7 @@ var app = &cli.App{
 			return retryablehttp.DefaultRetryPolicy(ctx, resp, nil)
 		}
 
-		tlsTransp, err := tlsclient.NewTransportWithOptions(tls_client.NewNoopLogger(), tls_client.WithClientProfile(profiles.Firefox_135))
+		tlsTransp, err := tlsclient.NewTransportWithOptions(tls_client.NewNoopLogger(), tls_client.WithClientProfile(profiles.Chrome_146_PSK))
 		if err != nil {
 			return fmt.Errorf("create tls transport: %w", err)
 		}
